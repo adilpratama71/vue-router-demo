6ed16d452dc139a7ffcfc023316b6d8f450610fa
Priambodo-Kurniawan <priambodo.2009@gmail.com>
2020-03-17 12:38:50 +0700

    list card routing

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -2,11 +2,11 @@
   <div class="col-md-3">
     <div class="card mb-3 shadow-sm text-center">
       <div class="p-3">
-        <img class="img-fluid" :src="getImage(index)" alt="">
+        <img class="img-fluid" :src="data.imageUrl ? data.imageUrl : image" alt="">
       </div>
       <div class="card-body">
         <span class="text-capitalize">{{data.name}}</span>
-        <div class="mt-3">
+        <div class="mt-3" v-if="!data.id">
           <button class="btn btn-secondary btn-sm" @click="goToDetailPokemon">View Detail</button>
         </div>
       </div>
@@ -17,13 +17,15 @@
 <script>
 export default {
   props: ['data', 'index'],
-  methods: {
-    getImage (index) {
-      const s = '000' + (index + 1)
-      const indexLength = (index + 1).toString().split('').length
+  computed: {
+    image () {
+      const s = '000' + (this.index + 1)
+      const indexLength = (this.index + 1).toString().split('').length
       const imageName = s.substr(indexLength)
       return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${imageName}.png`
-    },
+    }
+  },
+  methods: {
     goToDetailPokemon () {
       this.$router.push({ name: 'DetailPokemon', params: { name: this.data.name } })
     }
--- client/src/views/ListCard.vue
+++ client/src/views/ListCard.vue
@@ -1,5 +1,51 @@
 <template>
   <div>
-    <h2>List Card</h2>
+    <div v-if="!isLoading">
+      <h2>List Card</h2>
+      <div class="row">
+        <Card v-for="card in cards" :key="card.id" :data="card" />
+      </div>
+    </div>
+    <div v-else>
+      <Loading />
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import Card from '../components/Card'
+import Loading from '../components/Loading'
+export default {
+  name: 'ListCard',
+  data () {
+    return {
+      cards: [],
+      isLoading: false
+    }
+  },
+  components: {
+    Card,
+    Loading
+  },
+  methods: {
+    getCards () {
+      this.isLoading = true
+      axios.get('https://api.pokemontcg.io/v1/cards?pageSize=20&page=1')
+        .then(response => {
+          const { data } = response
+          this.cards.push(...data.cards)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        .finally(_ => {
+          this.isLoading = false
+        })
+    }
+  },
+  created () {
+    this.getCards()
+  }
+}
+</script>
